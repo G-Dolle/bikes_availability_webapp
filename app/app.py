01c46fcbc21814c2e9e8d6d21254d92afdbd7a02
guillaume dolle <guillaume.doll4@gmail.com>
2024-06-23 15:11:24 +0200

    Cleaned comments in the app.py script

--- app/app.py
+++ app/app.py
@@ -7,11 +7,6 @@ import folium
 from folium.plugins import MarkerCluster
 from streamlit_folium import folium_static
 
-#st.set_page_config(
-##    page_title="Web app",
- #   page_icon="🌍",
-  #  layout="centered",  # wide
- #   initial_sidebar_state="auto")
 
 #Title of the app
 st.title("THIS WEB APP PREDICTS THE AVAILABILITY OF DIVVY BIKES UP TO FIVE DAYS IN ADVANCE")
@@ -29,7 +24,7 @@ max_date = now + datetime.timedelta(days=5)
 # Obtain the user inputs
 with st.form('Please provide the inputs'):
     selected_date = st.date_input('Select a date', value=now.date(), min_value=now.date(), max_value=max_date.date())
-    selected_time = st.time_input('Select a time', value=now.time())
+    selected_time = st.time_input('Select a time')
     submit_button = st.form_submit_button("Submit")
 
     # Check if the selected date and time are valid
@@ -66,24 +61,20 @@ with st.form('Please provide the inputs'):
         st.header("Here is bikes availability per station on the selected date and time")
 
 
-
-        # Step 3: Load the dataframe with latitude, longitude, and dummy_value variables
-        # Replace 'df' with the name of your dataframe
-
-        # Step 4: Create a map object
+        # Create a map object
         m = folium.Map(location=[stations['lat'].mean(), stations['lng'].mean()], zoom_start=12)
 
-        # Step 5: Create a MarkerCluster object
+        # Create a MarkerCluster object
         marker_cluster = MarkerCluster(disableClusteringAtZoom=1).add_to(m)
 
-        # Step 6: Add markers to the map
+        # Add markers to the map
         for index, row in stations.iterrows():
             coord = (row['lat'], row['lng'])
             color = 'red' if row['result'] == 1 else 'green'
             label = f'<div style="font-size: 14px;">{row["station_name"]}</div>'
             folium.Marker(coord, icon=folium.Icon(color=color), popup=label).add_to(marker_cluster)
 
-        # Step 7: Fit the map bounds to show all markers
+        # Fit the map bounds to show all markers
         m.fit_bounds(marker_cluster.get_bounds())
 
         folium_static(m)
